60d9bc22197ed15ed08357f315be3083d30de58d
smagul.zh <smagul.zh@DRIPIS-P>
2018-12-11 09:30:43 +0600

    Added Required attributes to Models/GuestResponse

--- Models/GuestResponse.cs
+++ Models/GuestResponse.cs
@@ -1,10 +1,20 @@
-﻿namespace PartyInvites.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvites.Models
 {
     public class GuestResponse
     {
+        [Required(ErrorMessage= "Пожалуйста, введите свое имя")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, введите email")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Вы ввели некорректный email")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, введите телефон")]
         public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, укажите, примите ли участие в вечеринке")]
         public bool? WillAttend { get; set; }
     }
 }
